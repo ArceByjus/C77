8f692613959df2274b791d284ff21e5fcbaaef6d
ArceByjus <134817484+ArceByjus@users.noreply.github.com>
2023-09-02 18:49:57 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("Hola mundo!");
+
+var nombre1 = "Juan";
+var nombre2 = "Perez";
+var nombreCompleto = nombre1.concat(" ", nombre2);
+console.log(nombreCompleto);
+
+var x = 20;
+var y = 350;
+console.log(x, ' * ', y, ' = ',x*y);
